b441a59232c77405db317110e80ac1bce0fb32db
Angelina Wong <angelina.w25@gmail.com>
2018-05-18 19:17:50 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize(new)
+    @total = total
+  end
+  
+  def add_item
+    
+  end
+  
+  def apply_discount
+    
+  end
+  
+  def total
+    
 
+  end
+  
+  
+  def void_last_transaction
+    
+  end
+
+end
